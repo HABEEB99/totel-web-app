93d830f6fd5cee1895fedb5bdd468fb2a0e589ad
Habeeb <habeebahmadu1@gmail.com>
2023-06-03 23:07:58 +0100

    feat: add location data

---
+++ src/lib/data.ts
@@ -0,0 +1,85 @@
+export const locations = [
+  {
+    id: 1,
+    status: "Published",
+    name: "Devid Johanson",
+    isVerified: true,
+    age: "23 Years / Male",
+    date: "Available From - 28 Apr 2023",
+    details:
+      "Looking for 1-2 room mates for a place in Manchester city centre (wanting to move in around August time).",
+    location: "Bristol, Bath & London",
+    price: "$320-$420 / Month",
+    imageUrl: "/image1.png",
+  },
+
+  {
+    id: 2,
+    status: "Unpublished",
+    name: "Devid Johanson",
+    isVerified: true,
+    age: "23 Years / Male",
+    date: "Available From - 28 Apr 2023",
+    details:
+      "Looking for 1-2 room mates for a place in Manchester city centre (wanting to move in around August time).",
+    location: "Bristol, Bath & London",
+    price: "$320-$420 / Month",
+    imageUrl: "/image1.png",
+  },
+
+  {
+    id: 3,
+    status: "Published",
+    name: "Hotel Assenzio Karoeke",
+    isVerified: false,
+    rating: 4.2,
+    date: "25 Apr 2023 - 28 Apr 2023",
+    details:
+      "Looking for 1-2 room mates for a place in Manchester city centre (wanting to move in around August time).",
+    location: "Bristol, Bath & London",
+    price: "$320-$420 / Month",
+    imageUrl: "/image2.png",
+  },
+
+  {
+    id: 4,
+    status: "Published",
+    name: "Devid Johanson",
+    isVerified: true,
+    age: "23 Years / Male",
+    date: "Available From - 28 Apr 2023",
+    details:
+      "Looking for 1-2 room mates for a place in Manchester city centre (wanting to move in around August time).",
+    location: "Bristol, Bath & London",
+    price: "$320-$420 / Month",
+    imageUrl: "/image1.png",
+  },
+
+  {
+    id: 5,
+    status: "Published",
+    name: "Hotel Assenzio Karoeke",
+    isVerified: false,
+    rating: 4.2,
+    date: "25 Apr 2023 - 28 Apr 2023",
+    details:
+      "Looking for 1-2 room mates for a place in Manchester city centre (wanting to move in around August time).",
+    location: "Bristol, Bath & London",
+    price: "$320-$420 / Month",
+    imageUrl: "/image2.png",
+  },
+
+  {
+    id: 6,
+    status: "Unpublished",
+    name: "Devid Johanson",
+    isVerified: true,
+    age: "23 Years / Male",
+    date: "Available From - 28 Apr 2023",
+    details:
+      "Looking for 1-2 room mates for a place in Manchester city centre (wanting to move in around August time).",
+    location: "Bristol, Bath & London",
+    price: "$320-$420 / Month",
+    imageUrl: "/image1.png",
+  },
+];
